e2b328fdb1e501d650ad1a8289769972f3086125
Allan dos Santos <71888075+allan-gh@users.noreply.github.com>
2021-03-12 19:15:56 -0300

    Add files via upload

---
+++ segundo-app/src/HomePage/Index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css'
+
+
+import Cards from '../Cards/Index'
+
+export default function Homepage() {
+    return (
+        <div id='container-homepage'>
+            <h3>Cursos em alta no Senai</h3>
+            <Cards />
+
+        </div>
+
+    );
+
+
+
+
+}
